dbe3650176749ce15ab3e22522b9fb5c10cf932c
Jeff P <jeff@siccr.com>
2018-01-25 12:30:18 -0600

    include thank you message as OK compare

--- src/main/java/org/orcid/mailgun/Check.java
+++ src/main/java/org/orcid/mailgun/Check.java
@@ -37,7 +37,7 @@ public class Check
         try {
                 JsonNode r = new MailgunAuth().sendTestMessage();
                 JSONObject firstval = r.isArray()?r.getArray().getJSONObject(0):r.getObject();
-                if(firstval.getString("message").indexOf("Great job") != -1 || firstval.getString("message").indexOf("Thank you") ) {
+                if(firstval.getString("message").indexOf("Great job") != -1 || firstval.getString("message").indexOf("Thank you") != -1 ) {
                         System.out.println("OK: Test email sent.");
                         System.exit(0);
                 }
