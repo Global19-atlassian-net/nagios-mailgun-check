674f8713e259531268f7786027322e0ab904f0c6
Jeff P <jeff@siccr.com>
2018-01-26 13:22:12 -0600

    include valid to

--- src/main/java/org/orcid/mailgun/Check.java
+++ src/main/java/org/orcid/mailgun/Check.java
@@ -21,7 +21,7 @@ class MailgunAuth
         HttpResponse<JsonNode> request = Unirest.post(config.getProperty("com.mailgun.notify.apiUrl"))
                 .basicAuth("api", config.getProperty("com.mailgun.apiKey"))
                 .queryString("from", "me@notify.orcid.org")
-                .queryString("to", "you@ost.orcid.org")
+                .queryString("to", "test@orcid.org")
                 .queryString("subject", "check")
                 .queryString("text", "mailgun checking...")
                 .field("o:testmode", true)
